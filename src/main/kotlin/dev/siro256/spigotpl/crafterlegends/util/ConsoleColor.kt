17aaf1755115125aa66af94b9a9a635c7e427702
Siro_256 <kai0917@outlook.jp>
2020-10-22 21:02:14 +0900

    Create ConsoleColor.kt
    Windowsのコンソールはゴミ

---
+++ src/main/kotlin/dev/siro256/spigotpl/crafterlegends/util/ConsoleColor.kt
@@ -0,0 +1,26 @@
+package dev.siro256.spigotpl.crafterlegends.util
+
+@Suppress("unused")
+object ConsoleColor {
+    const val BLACK = "\u001B[30m"
+    const val DARK_BLUE = "\u001B[34m"
+    const val DARK_GREEN = "\u001B[32m"
+    const val DARK_AQUA = "\u001B[36m"
+    const val DARK_RED = "\u001B[31m"
+    const val DARK_PURPLE = "\u001B[35m"
+    const val GOLD = "\u001B[33m"
+    const val GRAY = "\u001B[37m"
+    const val DARK_GRAY = "\u001B[1;30m"
+    const val BLUE = "\u001B[1;34m"
+    const val GREEN = "\u001B[1;32m"
+    const val AQUA = "\u001B[1;36m"
+    const val RED = "\u001B[1;31m"
+    const val LIGHT_PURPLE = "\u001B[1;35m"
+    const val YELLOW = "\u001B[1;33m"
+    const val WHITE = "\u001B[1;37m"
+    const val BOLD = "\u001B[1m"
+    const val STRIKETHROUGH = "\u001B[9m"
+    const val UNDERLINE = "\u001B[4m"
+    const val ITALIC = "\u001B[3m"
+    const val RESET = "\u001B[0m"
+}
